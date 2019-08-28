b47f4c18562b397f3835b249071bc8c564296187
Arpan Patel <adp8ke@gmail.com>
2019-08-28 15:03:53 -0400

    8 done

--- 5-12.js
+++ 5-12.js
@@ -46,3 +46,39 @@
 // }
 
 // console.log(findMax([4, 6, -3, 5, -2, 1]))
+
+
+function mergeArrays(arr1, arr2){
+
+    let merged = arr1.concat(arr2).sort(function(a, b){return a-b});
+
+    return merged;
+
+    // let greater = 0;
+
+    // for (let i = 0; i < merged.length; i++){
+
+    //     console.log('i',merged[i])
+
+    //     for(let j = 0; j < merged.length; j++){
+
+    //         console.log('j', merged[j])
+
+    //         if (merged[i] < merged[j+1]){
+
+    //             // greater = merged[i]
+    //             // merged[i] = merged[j+1]
+    //             // merged[j+1] = greater;
+
+    //             greater = merged[j+1]
+    //             merged[j+1] = merged[i]
+    //             merged[i] = greater;
+    //         }
+    //     }
+    // }
+
+    // return merged;
+
+}
+
+console.log(mergeArrays([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]))
