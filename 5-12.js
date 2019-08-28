e163f7b58e1851d718b359d2ef644dd625c77682
Arpan Patel <adp8ke@gmail.com>
2019-08-28 14:34:20 -0400

    5-7 done

---
+++ 5-12.js
@@ -0,0 +1,48 @@
+// function URLify(string){
+
+//     let newString = string.replace(/ /gi, '%20')
+
+//     return newString
+// }
+
+// console.log(URLify('tauhida parveen'))
+// console.log(URLify('www.thinkful.com /tauh ida parv een'))
+
+
+// function filter(arr){
+
+//     let filtered = []
+
+//     for (let i = 0; i < arr.length; i++){
+//         if(arr[i] < 5){
+//             filtered.push(arr[i])
+//         }
+//     }
+
+//     return filtered
+
+// }
+
+// console.log(filter([1, 2, 3, 5, 6, 7, 8]))
+
+
+// function findMax(arr){
+
+//     let max = 0;
+
+//     let current = 0;
+
+//     for(let i = 0; i < arr.length; i++){
+
+//         current = current + arr[i]
+
+//         if(current > max){
+//             max = current
+//         }
+//     }
+
+//     return max
+
+// }
+
+// console.log(findMax([4, 6, -3, 5, -2, 1]))
