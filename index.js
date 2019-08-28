4cc3f38a63707db7bbac220171a7e8b465809d4c
Arpan Patel <adp8ke@gmail.com>
2019-08-28 14:05:12 -0400

    4 done

--- index.js
+++ index.js
@@ -75,22 +75,26 @@ function main(){
 
     let arr = new Array()
 
-    arr.push(3)
-    arr.push(5);
-    arr.push(15);
-    arr.push(19);
-    arr.push(45);
-    arr.push(10);
+    // arr.push(3)
+    // arr.push(5);
+    // arr.push(15);
+    // arr.push(19);
+    // arr.push(45);
+    // arr.push(10);
 
-    arr.pop();
-    arr.pop();
-    arr.pop();
+    // arr.pop();
+    // arr.pop();
+    // arr.pop();
+
+    arr.push("tauhida");
 
     console.log(arr)
+
+    console.log(arr.get(0))
 }
 
 main()
 
-//2. The length of the array is 1, the capacity is 3, and the address is 0. The length now becomes 6, capacity becomes 12, and pointer address becomes 3. At push(19) the length of the array is >= the capacity, so the resize function is called and the capacity now becomes (3+1)*4 = 12. The pointer then points to a new address where the index is 0.
-//3. The length of the array i 3, the capacity is 12, and the address is 3. The pop method takes the value based on the code and descreases the length of the array, then returns the value that was "popped". It will first remove the value of 10, then decrease the length of 1. Then do the same for the last two in the array. The pointer and capacity do not change because the length is still less than the capacity.
-//4. 
+//2. The length of the array is 1, the capacity is 3, and the address is 0. The length now becomes 6, capacity becomes 12, and pointer address becomes 3. At push(19) the length of the array is >= the capacity, so the resize function is called and the capacity now becomes (3+1)*4 = 12. The pointer then points to a new address where the array begins
+//3. The length of the array i 3, the capacity is 12, and the address is 3. The pop method takes the value based on the code and descreases the length of the array, then returns the value that was "popped". It will first remove the value of 10, then decrease the length of 1. Then do the same for the last two in the array. The pointer and capacity do not change because the resize function is not called.
+//4. The reason arr[0] is NaN is due to the float64array. The purpose of the _resize() is to be a private function that should only be accessed in the array class. Otherwise, its function is adjust the size of the memory and capacity
